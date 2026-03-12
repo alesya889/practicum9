f4b0acb3416562e8d011b429354112d6b2145ef2
alesya <a.davydova5@g.nsu.ru>
2026-03-12 21:35:43 +0700

    Update ex3.py

--- PythonProject9/ex3.py
+++ PythonProject9/ex3.py
@@ -1,7 +1,8 @@
-with open('input.txt', 'r', encoding='utf-8') as f, \
-        open('output.txt', 'w', encoding='utf-8') as f2:
+with open('input2.txt', 'r', encoding='utf-8') as f, \
+        open('output2.txt', 'w', encoding='utf-8') as f2:
     firstlet = ''
 
     for line in f:
         firstlet = firstlet + line[0]
+
     f2.write(firstlet)
