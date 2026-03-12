9bdb2803a2860288222274bf18427c4a93bc157b
alesya <a.davydova5@g.nsu.ru>
2026-03-12 21:35:02 +0700

    Update ex2.py

--- PythonProject9/ex2.py
+++ PythonProject9/ex2.py
@@ -1,6 +1,7 @@
-with open('input.txt', 'r', encoding='utf-8') as f, \
-        open('output.txt', 'w', encoding='utf-8') as f2:
+with open('input2.txt', 'r', encoding='utf-8') as f, \
+        open('output2.txt', 'w', encoding='utf-8') as f2:
 
     for line in f:
         if line[0] == 'A' or line[0] == 'a':
+
             f2.write(line)
