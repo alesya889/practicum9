f58b58ecc66d3711b881529de024ebb4ec89d350
alesya <a.davydova5@g.nsu.ru>
2026-03-12 21:36:14 +0700

    Update ex4.py

--- PythonProject9/ex4.py
+++ PythonProject9/ex4.py
@@ -1,6 +1,7 @@
-with open('input.txt', 'r', encoding='utf-8') as f, \
-        open('output.txt', 'w', encoding='utf-8') as f2:
+with open('input2.txt', 'r', encoding='utf-8') as f, \
+        open('output2.txt', 'w', encoding='utf-8') as f2:
 
     for line in f:
         if len(line) > 20:
+
             f2.write(line)
