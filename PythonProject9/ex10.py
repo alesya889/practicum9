a518e2f77dbc6dd3725ce1288eda0c4a6b436539
alesya <a.davydova5@g.nsu.ru>
2026-03-12 21:33:54 +0700

    Update ex10.py

--- PythonProject9/ex10.py
+++ PythonProject9/ex10.py
@@ -4,7 +4,7 @@ with (open('input.txt', 'r', encoding='utf-8') as f, \
     tdday = lines[0].split('.')[0]
     tdmonth = lines[0].split('.')[1].rstrip('\n\r')
     datas = []
-
+    
     for line in lines[2:]:
         linespl = line.split()
         dat = linespl[-1]
@@ -19,6 +19,32 @@ with (open('input.txt', 'r', encoding='utf-8') as f, \
                 and (abs(int(day) - int(tdday)) > 4)):
                     f2.write(num + '\n')
                     break
+            elif ((abs(int(month) - int(tdmonth)) == 1)
+            or (tdmonth == '01' and month == '12')):
+                if str(month) in ['1', '3', '5', '7', '8', '10', '12']:
+                    if ((day == '30' and tdday == '01') or
+                    (day == '31' and tdday == '02') or
+                    (day == '31' and tdday == '01')):
+                        break
+                    else:
+                        f2.write(num + '\n')
+                        break
+                elif (month == '02' and tdmonth == '03'):
+                    if ((day == '27' and tdday == '01') or 
+                    (day == '28' and tdday == '01') or
+                    (day == '28' and tdday == '02')):
+                        break
+                    else:
+                        f2.write(num + '\n')
+                        break
+                else:
+                    if ((day == '29' and tdday == '01') or
+                    (day == '30' and tdday == '01') or 
+                    (day == '30' and tdday == '02')):
+                        break
+                    else:
+                        f2.write(num + '\n')
+                        break
             else:
                 f2.write(num + '\n')
                 break
